7aede1f58695631d90a189d11624693dc78fdd84
itzsirsettings <technologyupgrade1@gmail.com>
2026-05-22 00:28:11 +0100

    fix: add missing Link import in PublicProofSection.tsx

--- components/landing/PublicProofSection.tsx
+++ components/landing/PublicProofSection.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from "framer-motion";
 import { useEffect, useMemo, useState } from "react";
+import Link from "next/link";
 
 type PlatformMetrics = {
   processedAmount: number;
